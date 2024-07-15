0f659cb9cacdca682297baa0531c862d298f7609
egor <ist_ea@mail.ru>
2024-07-15 12:40:33 +0700

    version_for_debug

--- PictureBackgroundSegmentation/make_mask.cpp
+++ PictureBackgroundSegmentation/make_mask.cpp
@@ -11,26 +11,27 @@ Mat background_segmentation_by_knn(Mat img, vector<vector<int>> init_pixels, int
     if (k > init_pixels.size()) {
         k = init_pixels.size();
     }
-    int num_threads = 20;
-    vector<vector<int>> distances_t(num_threads);
+    /*int num_threads = 6000;
+    vector<vector<float>> distances_t(num_threads);
     for (int i = 0; i < num_threads; i++) {
-        distances_t[i] = vector<int>(init_pixels.size());
-    }
-    parallel_for(int(0), int(im.rows), [&](int i) {
-        vector<float> distances_with_init_pixels(init_pixels.size());
+        distances_t[i] = vector<float>(init_pixels.size());
+    }*/
+    vector<double> distances(init_pixels.size());
+    double distance;
+    for (int i = 0; i < im.rows; i++) {
         for (int j = 0; j < im.cols; j++) {
             Vec3b now_color = im.at<Vec3b>(i, j);
             for (int d = 0; d < init_pixels.size(); d++) {
-                float distance =
-                    ((now_color[0] - init_pixels[d][3]) * (now_color[0] - init_pixels[d][3])) +
-                    ((now_color[1] - init_pixels[d][4]) * (now_color[1] - init_pixels[d][4])) +
+                distance =
+                    ((now_color[0] - init_pixels[d][3]) * (now_color[0] - init_pixels[d][3])) + 
+                    ((now_color[1] - init_pixels[d][4]) * (now_color[1] - init_pixels[d][4])) + 
                     ((now_color[2] - init_pixels[d][5]) * (now_color[2] - init_pixels[d][5]));
-                distances_with_init_pixels[d] = distance + 0.1 * init_pixels[d][2];
+                distances[d] = distance + (0.1 * init_pixels[d][2]);
             }
-            partial_sort(distances_with_init_pixels.begin(), distances_with_init_pixels.begin() + k, distances_with_init_pixels.end());
+            partial_sort(distances.begin(), distances.begin() + k, distances.end());
             int m[2] = { 0, 0 };
             for (int c = 0; c < k; c++) {
-                m[int((distances_with_init_pixels[c] - int(distances_with_init_pixels[c])) * 10)] += 1;
+                m[int((distances[c] - int(distances[c])) * 10)] += 1;
             }
             if (m[0] > m[1]) {
                 im.at<Vec3b>(i, j) = Vec3b(0, 0, 0);
@@ -39,7 +40,7 @@ Mat background_segmentation_by_knn(Mat img, vector<vector<int>> init_pixels, int
                 im.at<Vec3b>(i, j) = Vec3b(255, 255, 255);
             }
         }
-        });
+    }
 
     return im;
 }
