f606fe7ab3f7371cab7540b03c8dede49dd76e91
egor <ist_ea@mail.ru>
2024-07-17 15:56:22 +0700

    version_with_avx

--- PictureBackgroundSegmentation/make_mask.cpp
+++ PictureBackgroundSegmentation/make_mask.cpp
@@ -23,29 +23,20 @@ Mat background_segmentation_by_knn(Mat img, vector<vector<int>> init_pixels, int
     auto thread_func = [init_pixels, k, num_threads](Mat im, int thread_id, int distance, vector<float> distances, vector<int> m) {
         int i = im.rows / num_threads * thread_id;
         int i_fin = im.rows / num_threads * (thread_id + 1);
-        int r, g, b, r_ip, g_ip, b_ip;
+        __m128i i_c, n_c, minuss;
         for (; i < i_fin; i++) {
             for (int j = 0; j < im.cols; j++) {
                 m = { 0, 0 };
                 Vec3b now_color = im.at<Vec3b>(i, j);
-                r = now_color[0];
-                g = now_color[1];
-                b = now_color[2];
-                __m128i n_c = _mm_set_epi32(r, g, b, 0);
+                n_c = _mm_set_epi32(now_color[0], now_color[1], now_color[2], 0);
                 for (int d = 0; d < init_pixels.size(); d++) {
-                    r_ip = init_pixels[d][3];
-                    g_ip = init_pixels[d][4];
-                    b_ip = init_pixels[d][5];
-                    __m128i i_c = _mm_set_epi32(r_ip, g_ip, b_ip, 0);
-                    __m128i minuss = _mm_sub_epi32(n_c, i_c);
-                    __m128i m_c = minuss;
-                    __m128i for_sum = _mm_mul_epi32(minuss, m_c);
-                    int* result = (int*)&for_sum;
+                    i_c = _mm_set_epi32(init_pixels[d][3], init_pixels[d][4], init_pixels[d][5], 0);
+                    minuss = _mm_sub_epi32(n_c, i_c);
+                    int* minusss = (int*)&minuss;
                     distance =
-                        result[0] +
-                        result[1] +
-                        result[2];
-                    cout << distance << "\n";
+                        minusss[3] * minusss[3] +
+                        minusss[2] * minusss[2] +
+                        minusss[1] * minusss[1];
                     distances[d] = distance + 0.1 * init_pixels[d][2];
                 }
                 partial_sort(distances.begin(), distances.begin() + k, distances.end());
